ef1f5dc7f7010318324401fadc3ba60e1a13ebd5
The73756 <the73756the@gmail.com>
2023-07-13 16:03:30 +0900

    Add steps block

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -1,7 +1,8 @@
 import { Accordion } from "../functions/accordion";
 
 const positionAccordions = document.querySelectorAll(".positions__accordion");
+const stepsAccordions = document.querySelectorAll(".steps__accordion");
 
-positionAccordions.forEach((item) => {
+[...stepsAccordions, ...positionAccordions].forEach((item) => {
   new Accordion(item);
 });
